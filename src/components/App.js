8cea0bdf67f1085c66d12c7be90749015bcb4921
Juan P <jpmontoya182@gmail.com>
2019-10-07 08:54:25 -0500

    it was added a new component called search

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Header from "./header/Header";
+import Search from "./search/Search";
 
 function App() {
   return (
     <div className="App">
       <Header text="Buscador" />
+      <Search />
     </div>
   );
 }
